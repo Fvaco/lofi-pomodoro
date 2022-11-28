673418e0ebb5a7e6838100cf5334e55edaf401bc
Iván Hernández <ivan.hernandez@docplanner.com>
2022-11-28 17:53:47 +0100

    chore: extract some constants

---
+++ app/constants.ts
@@ -0,0 +1,22 @@
+export enum PomodoroPhases {
+  Focus,
+  ShortBreak,
+  LongBreak,
+}
+export const POMODORO_SEQUENCE = [
+  PomodoroPhases.Focus,
+  PomodoroPhases.ShortBreak,
+  PomodoroPhases.Focus,
+  PomodoroPhases.ShortBreak,
+  PomodoroPhases.Focus,
+  PomodoroPhases.ShortBreak,
+  PomodoroPhases.Focus,
+  PomodoroPhases.LongBreak,
+];
+
+export const POMODORO_DURATION = {
+  [PomodoroPhases.Focus]: 25 * 60, // 25 minutes
+  [PomodoroPhases.ShortBreak]: 5 * 60, // 5 minutes
+  [PomodoroPhases.LongBreak]: 20 * 60, // 20 minutes
+};
+
